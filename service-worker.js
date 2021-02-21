c74cb59e84422158c07fe88a4eef38e2dc16c717
Rithik Makhija <rithikmakhija2012@gmail.com>
2021-02-21 10:54:19 +0400

    installation procedure

--- service-worker.js
+++ service-worker.js
@@ -20,3 +20,15 @@ self.addEventListener('install', (e) => {
         return cache.addAll(cacheFiles);
     }));
 });
+self.addEventListener('fetch', function(e) {
+    e.respondWith(caches.match(e.request).then(function(r) {
+        // Download the file if it is not in the cache, 
+        return r || fetch(e.request).then(function(response) {
+            // add the new file to cache
+            return caches.open(cacheName).then(function(cache) {
+                cache.put(e.request, response.clone());
+                return response;
+            });
+        });
+    }));
+});
