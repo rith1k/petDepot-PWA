b9ab9b8347f9066d6fcf631674274ed1bcc6f227
Rithik Makhija <rithikmakhija2012@gmail.com>
2021-02-21 11:28:18 +0400

    changed images

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,44 @@
-var cacheName = ' petstore-v1';
+var cacheName = 'petstore-v1';
 var cacheFiles = [
     'index.html',
     'products.js',
     'petstore.webmanifest',
-    'images/airJ.jpg',
-    'images/among-us.png',
-    'images/babyYoda.jpg',
-    'images/catfood.jpg',
-    'images/catH.jpg',
-    'images/download.png',
-    'images/icon.png'
-
+    'images/yarn.jpg',
+    'images/cat-litter.jpg',
+    'images/laser-pointer.jpg',
+    'images/cat-house.jpg',
+    'images/icon-store-512.png'
+    
 ];
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
-    e.waitUntil(caches.open(cacheName).then((cache) => {
-        console.log('[Service Worker] Caching all the files');
-        return cache.addAll(cacheFiles);
-    }));
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
 });
-self.addEventListener('fetch', function(e) {
+
+// self.addEventListener('fetch', function (e) {
+//     e.respondWith(
+//         // check if the cache has the file
+//         caches.match(e.request).then(function (r) {
+//             console.log('[Service Worker] Fetching resource: ' + e.request.url);
+//             // 'r' is the matching file if it exists in the cache
+//             return r 
+//         })
+//     );
+// });
+
+self.addEventListener('fetch', function (e) {
     e.respondWith(
-        caches.match(e.request).then(function(r) {
+        caches.match(e.request).then(function (r) {
             // Download the file if it is not in the cache, 
-            return r || fetch(e.request).then(function(response) {
+            return r || fetch(e.request).then(function (response) {
                 // add the new file to cache
-                return caches.open(cacheName).then(function(cache) {
+                return caches.open(cacheName).then(function (cache) {
                     cache.put(e.request, response.clone());
                     return response;
                 });
